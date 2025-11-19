01df9647ec6fa45960512c0bd531692e67b0ae51
bielwolf <gabryellobo@gmail.com>
2025-11-19 00:58:49 -0300

    refatorando restaurante e lisatgem dos pratos

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import IPrato from '../../../interfaces/IPrato';
+import axios from 'axios';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const [pratos, setPratos] = useState<IPrato[]>([]);
+
+  useEffect(() => {
+    axios.get(`http://0.0.0.0:8000/api/v1/${restaurante.id}/pratos/`)
+      .then(resposta => {
+        setPratos(resposta.data)
+      })
+  }, [restaurante.id]);
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
